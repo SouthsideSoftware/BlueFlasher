71baccc7e761b0c82cb1fb58fa8379e2f9a852bd
Tom Cabanski <tom@cabanski.com>
2016-12-10 08:33:07 -0600

    Basic example of blue flashing behavior

---
+++ BlueFlasher/Program.cs
@@ -0,0 +1,54 @@
+﻿using DelcomSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlueFlasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (1 == 1)
+            {
+                Console.WriteLine("Press <space> to flash blue.  Any other key to exit");
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Spacebar)
+                {
+                    Flash();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        static void Flash() { 
+            DelcomLight light = new DelcomLight();
+            try
+            {
+                //Here are all the possibilities with the three-color Delcom
+
+                //light.ChangeIndicator(DelcomIndicatorState.SolidGreen);
+                //light.ChangeIndicator(DelcomIndicatorState.FlashingGreen);
+                //light.ChangeIndicator(DelcomIndicatorState.Off);
+                //light.ChangeIndicator(DelcomIndicatorState.SolidRed);
+                //light.ChangeIndicator(DelcomIndicatorState.FlashingRed);
+                //light.ChangeIndicator(DelcomIndicatorState.Off);
+                //light.ChangeIndicator(DelcomIndicatorState.SolidBlue);
+                light.ChangeIndicator(DelcomIndicatorState.FlashingBlue);
+                
+                //keep it flashing for 20 seconds (20,000 milliseconds)
+                Thread.Sleep(20 * 1000);
+            }
+            finally
+            {
+                light.Dispose();
+            }
+        }
+    }
+}
